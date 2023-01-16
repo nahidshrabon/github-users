91c1a50cd59741ce8bae88392c0401c88f499d4e
nahidshrabon <nahidshrabon@gmail.com>
2023-01-17 03:30:09 +0600

    added unit tests setup

---
+++ github-users-api/github-users-apiTests/UsersViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  UsersViewModelTests.swift
+//  github-users-apiTests
+//
+//  Created by Md. Nahidul Islam on 17/1/23.
+//
+
+import XCTest
+@testable import github_users_api
+
+final class UsersViewModelTests: XCTestCase {
+    
+}
